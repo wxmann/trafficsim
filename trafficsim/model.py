c06233beda0f7a72b9219038b3ad1a426ae6d602
jitang <jim_tang@gap.com>
2017-04-19 15:16:52 -0700

    start setting up world orchestration

--- trafficsim/model.py
+++ trafficsim/model.py
@@ -5,21 +5,17 @@ VEHICLE_LENGTH = 4.8
 class VehicleState(object):
     @classmethod
     def undefined(cls):
-        return VehicleState(None, None, None, None, None)
+        return VehicleState(None, None, None)
 
-    def __init__(self, position, speed, lane, next_vehicle, prev_vehicle):
+    def __init__(self, position, speed, lane):
         self.position = position
         self.speed = speed
         self.lane = lane
-        self.next_vehicle = next_vehicle
-        self.prev_vehicle = prev_vehicle
 
     def coalesce(self, another_state):
         self.position = first_not_none(self.position, another_state.position)
         self.speed = first_not_none(self.speed, another_state.speed)
         self.lane = first_not_none(self.lane, another_state.lane)
-        self.next_vehicle = first_not_none(self.next_vehicle, another_state.next_vehicle)
-        self.prev_vehicle = first_not_none(self.prev_vehicle, another_state.prev_vehicle)
 
 
 def first_not_none(*args):
@@ -30,10 +26,12 @@ def first_not_none(*args):
 
 
 class Vehicle(object):
-    def __init__(self, position, target_speed, lane, next_vehicle=None, prev_vehicle=None):
+    def __init__(self, position, target_speed, lane, world):
         self.target_speed = target_speed
-        self._current_state = VehicleState(position, target_speed, lane, next_vehicle, prev_vehicle)
+        self._current_state = VehicleState(position, target_speed, lane)
         self._next_state = VehicleState.undefined()
+        lane.add(self)
+        self._world = world
 
     @property
     def speed(self):
@@ -59,92 +57,63 @@ class Vehicle(object):
     def lane(self, newlane):
         self._next_state.lane = newlane
 
-    @property
-    def vehicle_in_front(self):
-        return self._current_state.next_vehicle
-
-    @vehicle_in_front.setter
-    def vehicle_in_front(self, newveh):
-        self._next_state.next_vehicle = newveh
-
-    @property
-    def vehicle_behind(self):
-        return self._current_state.prev_vehicle
-
-    @vehicle_behind.setter
-    def vehicle_behind(self, newveh):
-        self._next_state.prev_vehicle = newveh
-
-    # only for testing purposes, not intended for public API consumption
-    def set_vehicles(self, veh_front, veh_behind):
-        self._current_state.next_vehicle = veh_front
-        self._current_state.prev_vehicle = veh_behind
-
     def next_predicted_position(self):
         return self.position + first_not_none(self._next_state.speed, self.speed)
-
-    def next_ideal_position(self):
-        return self.position + self.target_speed
-
-    def calculate(self):
-        if self.vehicle_in_front is not None:
-            ideal_next_position = self.next_ideal_position()
-            front_next_pos = self.vehicle_in_front.next_predicted_position()
-
-            if abs(ideal_next_position - front_next_pos) < VEHICLE_LENGTH:
-                if self.lane.left is not None and self.can_change_lane(self.lane.left):
-                    self.change_lane(self.lane.left)
-                elif self.lane.right is not None and self.can_change_lane(self.lane.right):
-                    self.change_lane(self.lane.right)
-                else:
-                    # brake to match the speed of the guy in front if you can't change lanes
-                    self.speed = self.vehicle_in_front.speed
-            else:
-                # cruise at target speed
-                self.speed = self.target_speed
-        self.cruise()
 
     def cruise(self):
         self.position = self.next_predicted_position()
 
+    def calculate(self):
+        next_lane, next_speed = self._decide_lane_and_speed()
+        self._world.register_change(self, next_lane, next_speed)
+
     def _vehicles_around_position(self, lane):
         return lane.first_vehicle_ahead(self.position), lane.first_vehicle_behind(self.position)
 
-    def change_lane(self, newlane):
-        newlane_veh_ahead, newlane_veh_behind = self._vehicles_around_position(newlane)
-        if newlane_veh_behind is not None:
-            newlane_veh_behind.vehicle_in_front = self
-        if newlane_veh_ahead is not None:
-            newlane_veh_ahead.vehicle_behind = self
-
-        if self.vehicle_in_front is not None:
-            self.vehicle_in_front.vehicle_behind = self.vehicle_behind
-        if self.vehicle_behind is not None:
-            self.vehicle_behind.vehicle_in_front = self.vehicle_in_front
-
-        self.speed = self.target_speed
-        self.lane = newlane
-
-    def can_change_lane(self, lane):
+    def _can_change_lane(self, lane):
         if lane is None:
             return False
 
-        newlane_veh_ahead, newlane_veh_behind = self._vehicles_around_position(lane)
-        this_veh_pos = self.next_ideal_position()
+        if lane is self.lane:
+            return True
 
-        if newlane_veh_ahead is None:
+        lane_veh_ahead, lane_veh_behind = self._vehicles_around_position(lane)
+        this_veh_pos = self.next_predicted_position()
+
+        if lane_veh_ahead is None:
             will_hit_vehicle_ahead = False
         else:
-            ahead_veh_pos = newlane_veh_ahead.next_predicted_position()
+            ahead_veh_pos = lane_veh_ahead.next_predicted_position()
             will_hit_vehicle_ahead = abs(ahead_veh_pos - this_veh_pos) < VEHICLE_LENGTH
 
-        if newlane_veh_behind is None:
+        if lane_veh_behind is None:
             behind_will_hit_you = False
         else:
-            behind_veh_pos = newlane_veh_behind.next_predicted_position()
+            behind_veh_pos = lane_veh_behind.next_predicted_position()
             behind_will_hit_you = abs(behind_veh_pos - this_veh_pos) < VEHICLE_LENGTH
 
         return not will_hit_vehicle_ahead and not behind_will_hit_you
+
+    def _decide_lane_and_speed(self):
+        speed_lanes_map = {}
+        for lane in self.lane, self.lane.left, self.lane.right:
+            if self._can_change_lane(lane):
+                speed_on_lane = self._max_speed_on(lane)
+                if speed_on_lane not in speed_lanes_map:
+                    speed_lanes_map[speed_on_lane] = []
+                speed_lanes_map[speed_on_lane].append(lane)
+
+        max_speed = max(speed_lanes_map)
+        # our lane will take priority over left lane will take priority over right lane
+        return speed_lanes_map[max_speed][0], max_speed
+
+    def _max_speed_on(self, lane):
+        lane_veh_ahead, _ = self._vehicles_around_position(lane)
+        if lane_veh_ahead is None:
+            return self.target_speed
+        else:
+            v_threshold = lane_veh_ahead.next_predicted_position() - self.position - VEHICLE_LENGTH
+            return min(v_threshold, self.target_speed)
 
     def apply(self):
         self._next_state.coalesce(self._current_state)
@@ -167,6 +136,9 @@ class Vehicle(object):
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+    def __hash__(self):
+        return id(self)
 
 
 class Lane(object):
@@ -205,3 +177,14 @@ class Lane(object):
             return self._vehicles[deltas.index(min_neg_delta)]
         except ValueError:
             return None
+
+
+class World(object):
+    def __init__(self):
+        pass
+
+    def register_change(self, veh, lane, speed):
+        pass
+
+    def orchestrate_step(self):
+        pass
